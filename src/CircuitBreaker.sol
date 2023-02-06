d209b80e70567a3fb66a9a3c986142feda196983
Philippe Dumonet <philippe@dumo.net>
2023-02-06 22:15:45 +0100

    🚧 Extend CB Interface

--- src/CircuitBreaker.sol
+++ src/CircuitBreaker.sol
@@ -4,4 +4,4 @@ pragma solidity 0.8.15;
 import {ICircuitBreaker} from "./interfaces/ICircuitBreaker.sol";
 import {CrispyERC1155} from "./CrispyERC1155.sol";
 
-contract CircuitBreaker is ICircuitBreaker, CrispyERC1155 {}
+abstract contract CircuitBreaker is ICircuitBreaker, CrispyERC1155 {}
--- src/interfaces/ICircuitBreaker.sol
+++ src/interfaces/ICircuitBreaker.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0-Only
 pragma solidity 0.8.15;
 
 import {IERC1155} from "@openzeppelin/token/ERC1155/IERC1155.sol";
@@ -6,8 +6,8 @@ import {IERC1155} from "@openzeppelin/token/ERC1155/IERC1155.sol";
 interface ICircuitBreaker is IERC1155 {
     enum GCBState {
         Running, // Circuit breaker is active and processing withdrawals
-        Blocked, // Settlement of pending and new withdrawals blocked
-        Frozen //
+        FrozenResolved, // Contract paused, but pending withdrawals marked and differentiated
+        LockedDown // Contract paused, pending withdrawals not settled
     }
 
     enum ReentrancyLock {
@@ -23,4 +23,24 @@ interface ICircuitBreaker is IERC1155 {
     }
 
     function state() external view returns (GCBState);
+
+    function queueETHTransfer(address payable recipient, uint256 amount) external returns (uint256);
+
+    function queueERC20Transfer(
+        address token,
+        address recipient,
+        uint256 amount
+    ) external returns (uint256);
+
+    function queuePayableCall(
+        address payable recipient,
+        uint256 amount,
+        bytes calldata data
+    ) external returns (uint256);
+
+    function queueCall(
+        address payable recipient,
+        uint256 amount,
+        bytes calldata data
+    ) external returns (uint256);
 }
