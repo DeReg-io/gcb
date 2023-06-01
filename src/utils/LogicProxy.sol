48fe868469aa97f73c31be1ffc1cd3c200de3d63
Philippe Dumonet <philippe@dumo.net>
2023-06-01 18:44:07 +0200

    ♻️ Clean up logic proxy

--- src/utils/LogicProxy.sol
+++ src/utils/LogicProxy.sol
@@ -1,38 +1,46 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity ^0.8.0;
 
 /// @author philogy <https://github.com/philogy>
 contract LogicProxy {
-    /// @dev ERC1967 implementation slot: `keccak256("eip1967.proxy.implementation") - 1`
-    /// Layout (data after implementation address considered "auxiliary"):
-    /// [  0-159] address implementation
-    /// [    160] "bit"   assetLayerActive
-    /// [    161] "bit"   app paused
+    /**
+     * @dev ERC1967 implementation slot: `keccak256("eip1967.proxy.implementation") - 1`
+     * Layout (data after implementation address considered "auxiliary"):
+     * [  0-159] address implementation
+     * [    160] "bit"   redirecting ETH (0 = disabled, 1 = enabled )
+     * [    161] "bit"   app paused      (0 = paused,   1 = unpaused)
+     */
     bytes32 internal constant _CORE_DATA_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
-    uint256 internal constant _LAYER_ACTIVE_FLAG = 0x010000000000000000000000000000000000000000;
-    uint256 internal constant _APP_ACTIVE_FLAG = 0x020000000000000000000000000000000000000000;
-    // Flags that cannot be touched by implementation
-    uint256 internal constant _ROOT_FLAGS_MASK = 0x020000000000000000000000000000000000000000;
+    uint256 internal constant _ETH_REDIRECT_DISABLED_FLAG = 0x010000000000000000000000000000000000000000;
+    uint256 internal constant _NOT_PAUSED_FLAG = 0x020000000000000000000000000000000000000000;
+    // Flags that cannot be touched by implementation (breaker disabled).
+    uint256 internal constant _ROOT_FLAGS_MASK = 0x010000000000000000000000000000000000000000;
     uint256 internal constant _AUX_ONLY_MASK = 0xffffffffffffffffffffffff0000000000000000000000000000000000000000;
 
-    address internal immutable assetLayer;
+    address internal immutable BREAKER;
 
-    error Paused();
-    error RootFlagChanged();
+    error CallWhilePaused();
+    error BreakerDisabled();
+    error RootFlagsChanged();
 
-    event AppPaused();
-    event AppUnpaused();
+    event Paused();
+    event Unpaused();
+    event EthRelayDisabled();
 
-    constructor(address _startImplementation) {
-        assetLayer = msg.sender;
+    constructor(address startImplementation, address breaker) {
+        assert(_ROOT_FLAGS_MASK == _ETH_REDIRECT_DISABLED_FLAG | 0);
+        BREAKER = breaker;
         assembly {
-            sstore(_CORE_DATA_SLOT, or(_startImplementation, _APP_ACTIVE_FLAG))
+            sstore(_CORE_DATA_SLOT, or(startImplementation, _NOT_PAUSED_FLAG))
         }
     }
 
-    /// @dev Reroutes call to implementation if caller not `assetLayer`
-    modifier onlyLayerCallable() {
-        if (msg.sender != assetLayer) {
+    /**
+     * @dev Reroutes call to implementation if caller not `BREAKER`. Allows use of same selector for
+     * other functions
+     */
+    modifier onlyBreaker() {
+        if (msg.sender != BREAKER) {
             _delegateToImpl();
         } else {
             _;
@@ -47,79 +55,88 @@ contract LogicProxy {
         _delegateToImpl();
     }
 
-    /// @dev Sets implementation if caller is `assetLayer`, forwards to
-    /// implementation otherwise incase it has its own `upgradeTo` method.
-    function upgradeTo(address _newImpl) external payable onlyLayerCallable {
+    /**
+     * @dev Allows `BREAKER` to upg
+     */
+    function upgradeTo(address _newImpl) external payable onlyBreaker {
         assembly {
             let currentImplData := sload(_CORE_DATA_SLOT)
             sstore(_CORE_DATA_SLOT, or(and(currentImplData, _AUX_ONLY_MASK), _newImpl))
         }
     }
 
-    function disableAssetLayer() external payable onlyLayerCallable {
+    function disableEthRelay() external payable onlyBreaker {
         assembly {
             let currentImplData := sload(_CORE_DATA_SLOT)
-            sstore(_CORE_DATA_SLOT, and(currentImplData, not(_LAYER_ACTIVE_FLAG)))
+            sstore(_CORE_DATA_SLOT, or(currentImplData, _ETH_REDIRECT_DISABLED_FLAG))
         }
+        emit EthRelayDisabled();
     }
 
-    function paused() external view returns (bool) {
+    function getProxyInfo()
+        external
+        view
+        returns (bool paused, bool breakerEnabled, address implementation, address breaker)
+    {
+        breaker = BREAKER;
         assembly {
-            mstore(0x00, iszero(and(sload(_CORE_DATA_SLOT), _APP_ACTIVE_FLAG)))
-            return(0x00, 0x20)
+            let coreData := sload(_CORE_DATA_SLOT)
+            paused := iszero(and(coreData, _NOT_PAUSED_FLAG))
+            breakerEnabled := iszero(and(coreData, _ETH_REDIRECT_DISABLED_FLAG))
+            implementation := coreData
         }
     }
 
-    function pauseApp() external payable onlyLayerCallable {
+    function pause() external payable onlyBreaker {
         assembly {
             let prevCoreData := sload(_CORE_DATA_SLOT)
-            sstore(_CORE_DATA_SLOT, and(prevCoreData, not(_APP_ACTIVE_FLAG)))
+            sstore(_CORE_DATA_SLOT, and(prevCoreData, not(_NOT_PAUSED_FLAG)))
         }
-        emit AppPaused();
+        emit Paused();
     }
 
-    function unpauseApp() external payable onlyLayerCallable {
+    function unpause() external payable onlyBreaker {
         assembly {
             let prevCoreData := sload(_CORE_DATA_SLOT)
-            sstore(_CORE_DATA_SLOT, or(prevCoreData, _APP_ACTIVE_FLAG))
+            sstore(_CORE_DATA_SLOT, or(prevCoreData, _NOT_PAUSED_FLAG))
         }
-        emit AppUnpaused();
+        emit Unpaused();
     }
 
-    /// @dev Forwards calldata, asset layer address and auxiliary data to the implementation sending any ETH to the `assetLayer`.
+    /// @dev Forwards calldata, asset layer address and auxiliary data to the implementation sending
+    //any ETH to the `BREAKER`.
     function _delegateToImpl() internal {
         // Store immutable locally since immutables not supported in assembly.
-        address assetLayer_ = assetLayer;
+        address breaker = BREAKER;
         assembly {
             let coreData := sload(_CORE_DATA_SLOT)
 
-            if iszero(and(coreData, _APP_ACTIVE_FLAG)) {
-                // `revert Paused()`
-                mstore(0x00, 0x9e87fac8)
+            if iszero(and(coreData, _NOT_PAUSED_FLAG)) {
+                // `revert CallWhilePaused()`
+                mstore(0x00, 0xa44edd3f)
                 revert(0x1c, 0x04)
             }
 
-            // Send ETH to layer for safe keeping if active and callvalue passed.
-            if iszero(or(iszero(callvalue()), and(_APP_ACTIVE_FLAG, coreData))) {
-                pop(call(gas(), assetLayer_, callvalue(), 0, 0, 0, 0))
+            // Send ETH to breaker for safe keeping if active and callvalue passed.
+            if iszero(or(iszero(callvalue()), and(coreData, _ETH_REDIRECT_DISABLED_FLAG))) {
+                pop(call(gas(), breaker, callvalue(), 0, 0, 0, 0))
             }
             // Copy calldata to memory.
             calldatacopy(0x00, 0x00, calldatasize())
             // Append asset layer and auxiliary data as an immutable arg.
-            mstore(calldatasize(), or(and(coreData, _AUX_ONLY_MASK), assetLayer_))
+            mstore(calldatasize(), or(and(coreData, _AUX_ONLY_MASK), breaker))
             let success := delegatecall(gas(), coreData, 0x00, add(calldatasize(), 0x20), 0x00, 0x00)
 
             // Ensure that implementation didn't change root flags.
             if iszero(eq(and(sload(_CORE_DATA_SLOT), _ROOT_FLAGS_MASK), and(coreData, _ROOT_FLAGS_MASK))) {
-                mstore(0x00, 0x0feb2b1a)
+                // `revert RootFlagsChanged()`
+                mstore(0x00, 0xe9df817e)
                 revert(0x1c, 0x04)
             }
 
             // Relay return data to caller.
             returndatacopy(0x00, 0x00, returndatasize())
-            if iszero(success) {
-                revert(0x00, returndatasize())
-            }
+            if iszero(success) { revert(0x00, returndatasize()) }
             return(0x00, returndatasize())
         }
     }
